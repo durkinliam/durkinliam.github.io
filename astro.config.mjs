c50691e26689ed1d3304901c77c469f4e70f5fb5
Liam Durkin <Liam.Durkin@skybettingandgaming.com>
2024-12-16 21:08:50 +0000

    Base Astro setup and initial commit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
+import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    site: 'https://durkinliam.github.io',
+    base: 'durkinliam.github.io',
+    integrations: [tailwind()]
 });
